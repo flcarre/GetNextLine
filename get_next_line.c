dd57542ec518db2bb2be47146455a3ea47c87ba1
Flwx <flcarre@student.42.fr>
2018-11-13 16:06:15 +0100

    fastpush

--- get_next_line.c
+++ get_next_line.c
@@ -6,42 +6,25 @@
 /*   By: flcarre <flcarre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 15:25:37 by flcarre           #+#    #+#             */
-/*   Updated: 2018/11/13 14:39:43 by flcarre          ###   ########.fr       */
+/*   Updated: 2018/11/13 16:00:21 by flcarre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <get_next_line.h>
 
-int	ft_file_size(int fd)
-{
-	int		i;
-	char	c;
-
-	i = 0;
-	while (read(fd, &c, 1))
-	{
-		write(1, &c, 1);
-		i++;
-	}
-	return(i);
-}
-
 char	*ft_file_str(int fd)
 {
 	int		i;
-	char	c;
+	char* c[BUFF_SIZE];
 	char *str;
 
-	str = malloc(400 * sizeof(char));
+	str = malloc(BUFF_SIZE + 1 * sizeof(char));
 	i = 0;
-	while (read(fd, &c, 1))
-	{
-		write(1, &c, 1);
-		str[i] = c;
-		i++;
-	}
+	read(fd, &c, BUFF_SIZE);
+	str = c;
+	str[BUFF_SIZE] = 0;
+	printf("%s\n", str);
 	close(fd);
-	return(i);
 }
 
 int get_next_line(const int fd, char **line)
@@ -52,6 +35,6 @@ int get_next_line(const int fd, char **line)
 
 int main(int argc, char const *argv[]) {
 	int fd = open(argv[1], O_RDONLY);
-	printf("%s\n", ft_file_str(fd));
+	ft_file_str(fd);
 	return 0;
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: flcarre <flcarre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 15:27:18 by flcarre           #+#    #+#             */
-/*   Updated: 2018/11/12 15:51:25 by flcarre          ###   ########.fr       */
+/*   Updated: 2018/11/13 16:04:55 by flcarre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include "libft/libft.h"
+# define BUFF_SIZE 1000
 
 int get_next_line(const int fd, char **line);
 
