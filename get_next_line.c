cd1800951558827a07835a3f48b2cc07f88c3672
Flwx <flcarre@student.42.fr>
2018-11-19 16:08:10 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -15,18 +15,20 @@
 char	*ft_full_str(int fd)
 {
 	int		i;
+	int end;
 	char* buff[BUFF_SIZE];
 	char *str;
 
-	//str = malloc(BUFF_SIZE - 1 * sizeof(char));
+	str = malloc(BUFF_SIZE - 1 * sizeof(char));
 	i = 0;
-	while(read(fd, &buff, BUFF_SIZE) > 0)
+	if ((end = read(fd, buff, BUFF_SIZE)) == -1)
+		return (-1);
+	str = ft_strjoin(str, buff);
+	while(end = read(fd, &buff, BUFF_SIZE))
 	{
 		i++;
-		ft_strjoin(buff, str);
-		printf("-%s\n", buff);
+		str = ft_strjoin(str, buff);
 	}
-	printf("___________%s\n", str);
 	str[BUFF_SIZE * i] = 0;
 	close(fd);
 	return(str);
@@ -37,9 +39,11 @@ int get_next_line(const int fd, char **line)
 	char **file;
 	char *str;
 	static int i;
-	//printf("%s\n", file[0]);
-	file = ft_strsplit(ft_full_str(fd), '\n');
-	//printf("i = %d %s\n",i, file[i]);
+    if (i == 0)
+	    file = ft_strsplit(ft_full_str(fd), '\n');
+	if (file[i] == NULL)
+	    return(0);
+	printf("i = %d %s\n",i, file[i]);
 	i++;
 }
 
@@ -51,8 +55,8 @@ int main(int argc, char const *argv[]) {
 	i = 0;
 	while(i < 3)
 	{
-	get_next_line(fd, &file);
-	i++;
+	    get_next_line(fd, &file);
+	    i++;
 	}
 	return 0;
 }
